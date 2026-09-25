28c4ec8571edc9a0775908e29d8fbe6b963e27ec
UnnamedDays <kjh3676518@uos.ac.kr>
2026-09-26 00:19:04 +0900

    Add files via upload

---
+++ LAB2/lab2_06_moore/src/lab2_moore.v
@@ -0,0 +1,35 @@
+`timescale 1ns/1ps
+
+module lab2_moore #(parameter integer STABLE_CYCLES = 20) (
+
+    input wire clk, input wire rst, input wire button, input wire [7:0] sw,
+    output wire [7:0] led
+
+);
+
+    wire reset, press;
+    wire [7:0] switches;
+
+    input_frontend #(
+        .STABLE_CYCLES(STABLE_CYCLES)
+    ) inputs (
+        clk,
+        rst,
+        button,
+        sw,
+        reset,
+        press,
+        switches
+    );
+
+    wire [1:0] value;
+
+    moore_cycle core(
+        clk,
+        reset,
+        press,
+        switches[7],
+        value
+    );
+    assign led = {6'b000000, value};
+endmodule
