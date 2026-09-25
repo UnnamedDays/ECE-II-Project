dcb298da6f3d28c062e39ce68b8ff1777c506de6
UnnamedDays <kjh3676518@uos.ac.kr>
2026-09-26 00:13:18 +0900

    Add files via upload

---
+++ LAB2/lab2_01_counter/src/counter4.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+
+module counter4(
+    input wire clk,
+    input wire rst,
+    input wire enable,
+    input wire down,
+    output reg [3:0] value
+);
+
+    always @(posedge clk) begin
+        if (rst)
+            value <= 4'd0;
+        else if (enable) begin
+            if (down)
+                value <= value - 4'd1;
+            else
+                value <= value + 4'd1;
+        end
+    end
+
+endmodule
